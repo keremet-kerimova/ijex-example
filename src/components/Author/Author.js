756eef99ff1bfc4677d1d2b0a91b1838addf62d2
Your Name <you@example.com>
2021-04-02 14:34:21 +0600

    adde and changing

---
+++ src/components/Author/Author.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import classes from "./Author.module.css";
+
+function Author(posts) {
+   
+    return (
+        <div className={classes.Author}>{user.Id}</div>
+    );
+  
+}
+export default Author;
+
---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Coments from "../Coments/Coments";
+import classes from "./FullPost.module.css";
+import Author from "../Author/Author";
+
+const FullPost = ({ id }) => {
+  const [post, setPost] = useState({
+    title: "Loading...",
+    body: "Wait for it!",
+    userId:0
+  });
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+      .then((response) => {
+        console.log(response);
+        const post = response.data;
+        setPost(post);
+      });
+  }, [id]);
+
+  return (
+    <div className={classes.FullPost}>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <Coments postId={id} />
+      <Author userId={ post.userId }
+    </div>
+  );
+}
+export default FullPost;
